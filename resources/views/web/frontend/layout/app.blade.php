0e69a6ce1ca6aaa654a968e812f568a313f60fa9
Alukardua1 <prizrack30@gmail.com>
2021-02-04 21:36:19 +0300

    03.02.2021 Добавлен bootstrap 5

--- resources/views/web/frontend/layout/app.blade.php
+++ resources/views/web/frontend/layout/app.blade.php
@@ -6,22 +6,34 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 	<title>@yield('title')</title>
 </head>
 <body>
-@section('sidebar')
-	This is the master sidebar.
-@show
+<header>
+	<navigate>
+		@include('web.frontend.layout.nav')
+	</navigate>
+	@section('sidebar')
+		This is the master sidebar.
+	@show
+</header>
+<main class="container-fluid">
+	<div class="row">
+		<div class="col-4">
+			@include('web.frontend.layout.menu')
+		</div>
+		<div class="col">
+			@yield('content')
+		</div>
+	</div>
+</main>
+<footer>
 
-<div class="container">
-	@include('web.frontend.layout.menu')
-
-
-
-
-	@yield('content')
-</div>
-
+</footer>
 <script src="{{ asset('js/app.js') }}" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/web/frontend/layout/menu.blade.php
+++ resources/views/web/frontend/layout/menu.blade.php
@@ -1,5 +1,7 @@
-@foreach($menu as $item)
-	<li @if($path == $item->url) class="active" @endif>
-		<a href="{{ route('currentCategory', $item->url) }}">{{ $item->title }}</a>
-	</li>
-@endforeach
+<div class="list-group list-group-flush">
+	@foreach($menu as $item)
+		<li class="list-group-item @if($path == "/category/".$item->url) active @endif">
+			<a href="{{ route('currentCategory', $item->url) }}">{{ $item->title }}</a>
+		</li>
+	@endforeach
+</div>
