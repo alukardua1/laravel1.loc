7158bf5438178a712946501f880afd4593e4997f
Alukardua1 <prizrack30@gmail.com>
2021-03-16 20:33:57 +0300

    Исправлен ComposerServiceProvider.php

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -37,18 +37,18 @@ class ComposerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-    	\View::composer('*', CategoryComposer::class);
-	    \View::composer('*', KindComposer::class);
-	    \View::composer('*', CarouselAnimeComposer::class);
-	    \View::composer('*', MpaaRatingComposer::class);
-	    \View::composer('*', TranslateComposer::class);
-	    \View::composer('*', YearComposer::class);
-	    \View::composer('*', CountryComposer::class);
-	    \View::composer('*', QualityComposer::class);
-	    \View::composer('*', AnonsComposer::class);
-	    \View::composer('*', PopularComposer::class);
-	    \View::composer('*', ChannelComposer::class);
-	    \View::composer('*', UserComposer::class);
-	    \View::composer('*', StudioComposer::class);
+    	\View::composer(['web.frontend.layout.component.menu', 'web.backend.anime.edit'], CategoryComposer::class);
+	    \View::composer(['web.frontend.layout.component.kind', 'web.backend.anime.edit'], KindComposer::class);
+	    \View::composer(['web.frontend.anime.component.carousel', 'web.backend.anime.edit'], CarouselAnimeComposer::class);
+	    \View::composer(['web.frontend.layout.component.mpaa', 'web.backend.anime.edit'], MpaaRatingComposer::class);
+	    \View::composer(['web.frontend.layout.component.translate', 'web.backend.anime.edit'], TranslateComposer::class);
+	    \View::composer(['web.frontend.layout.component.year', 'web.backend.anime.edit'], YearComposer::class);
+	    \View::composer(['web.frontend.layout.component.country', 'web.backend.anime.edit'], CountryComposer::class);
+	    \View::composer(['web.frontend.layout.component.quality', 'web.backend.anime.edit'], QualityComposer::class);
+	    \View::composer(['web.frontend.anime.component.anons'], AnonsComposer::class);
+	    \View::composer(['web.frontend.anime.component.popular'], PopularComposer::class);
+	    \View::composer('web.backend.anime.edit', ChannelComposer::class);
+	    \View::composer('web.backend.anime.edit', UserComposer::class);
+	    \View::composer('web.backend.anime.edit', StudioComposer::class);
     }
 }
